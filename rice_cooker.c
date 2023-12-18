6921b93a33e426042f795533881233ca8ed3aae2
Miaritiana <hei.miaritiana@gmail.com>
2023-12-19 00:27:42 +0300

    style: run linter

--- rice_cooker.c
+++ rice_cooker.c
@@ -2,101 +2,72 @@
 #include <stdio.h>
 #include <string.h>
 
+char *getName(struct Content *c) { return c->name; }
 
-char* getName(struct Content* c)
-{
-    return c->name;
+double getQuantity(struct Content *c) { return c->quantity; }
+
+void setName(struct Content *c, const char *newName) {
+  strncpy(c->name, newName, sizeof(c->name));
 }
 
-double getQuantity(struct Content* c)
-{
-    return c->quantity;
+void setQuantity(struct Content *c, double newQuantity) {
+  c->quantity = newQuantity;
 }
 
-void setName(struct Content* c, const char* newName)
-{
-    strncpy(c->name, newName, sizeof(c->name));
+double getCapacity(struct RiceCooker *rc) { return rc->capacity; }
+
+void setCapacity(struct RiceCooker *rc, double newCapacity) {
+  rc->capacity = newCapacity;
 }
 
-void setQuantity(struct Content* c, double newQuantity)
-{
-    c->quantity = newQuantity;
+struct Content getContent(struct RiceCooker *rc) {
+  return rc->content;
 }
 
-double getCapacity(struct RiceCooker* rc)
-{
-    return rc->capacity;
+void setContent(struct RiceCooker *rc, struct Content newContent) {
+  while (getQuantity(&newContent) > getCapacity(rc)) {
+    fprintf(stderr,
+            "Out of capacity. Please enter a quantity within capacity: ");
+    scanf("%lf", &newContent.quantity);
+  }
+
+  rc->content = newContent;
 }
 
-void setCapacity(struct RiceCooker* rc, double newCapacity)
-{
-    rc->capacity = newCapacity;
+void getContentDetails(struct RiceCooker *rc) {
+  printf("This rice-cooker contains %.2f kg of %s\n", getQuantity(&rc->content),
+         getName(&rc->content));
 }
 
-struct Content getContent(struct RiceCooker* rc)
-{
-    return rc->content;
+int getTimer(struct RiceCooker *rc) { return rc->timer; }
+
+void setTimer(struct RiceCooker *rc, int minute) {
+  rc->timer = minute;
+  printf("Timer is set to %d minutes\n", minute);
 }
 
-void setContent(struct RiceCooker* rc, struct Content newContent)
-{
-    while (getQuantity(&newContent) > getCapacity(rc))
-    {
-        fprintf(stderr, "Out of capacity. Please enter a quantity within capacity: ");
-        scanf("%lf", &newContent.quantity);
-    }
+char *getTemperature(struct RiceCooker *rc) { return rc->temperature; }
 
-    rc->content = newContent;
+void setTemperature(struct RiceCooker *rc, const char *setupTemperature) {
+  strncpy(rc->temperature, setupTemperature, sizeof(rc->temperature));
+  printf("Temperature set to %s\n", setupTemperature);
 }
 
-void getContentDetails(struct RiceCooker* rc)
-{
-    printf("This rice-cooker contains %.2f kg of %s\n", getQuantity(&rc->content), getName(&rc->content));
+int getCookingDuration(struct RiceCooker *rc) { return rc->cookingDuration; }
+
+void setCookingDuration(struct RiceCooker *rc, int minute) {
+  rc->cookingDuration = minute;
 }
 
-int getTimer(struct RiceCooker* rc)
-{
-    return rc->timer;
+void automaticCook(struct RiceCooker *rc) {
+  printf("Starting automatic cooking...\n");
+  printf("Cooking duration: %d minutes\n", getCookingDuration(rc));
+  printf("Temperature: %s\n", getTemperature(rc));
+  printf("Automatic cooking completed!\n");
 }
 
-void setTimer(struct RiceCooker* rc, int minute)
-{
-    rc->timer = minute;
-    printf("Timer is set to %d minutes\n", minute);
-}
-
-char* getTemperature(struct RiceCooker* rc)
-{
-    return rc->temperature;
-}
-
-void setTemperature(struct RiceCooker* rc, const char* setupTemperature)
-{
-    strncpy(rc->temperature, setupTemperature, sizeof(rc->temperature));
-    printf("Temperature set to %s\n", setupTemperature);
-}
-
-int getCookingDuration(struct RiceCooker* rc)
-{
-    return rc->cookingDuration;
-}
-
-void setCookingDuration(struct RiceCooker* rc, int minute)
-{
-    rc->cookingDuration = minute;
-}
-
-void automaticCook(struct RiceCooker* rc)
-{
-    printf("Starting automatic cooking...\n");
-    printf("Cooking duration: %d minutes\n", getCookingDuration(rc));
-    printf("Temperature: %s\n", getTemperature(rc));
-    printf("Automatic cooking completed!\n");
-}
-
-void manualCook(struct RiceCooker* rc)
-{
-    printf("Starting manual cooking...\n");
-    printf("Temperature: %s\n", getTemperature(rc));
-    printf("Manual cooking completed!\n");
+void manualCook(struct RiceCooker *rc) {
+  printf("Starting manual cooking...\n");
+  printf("Temperature: %s\n", getTemperature(rc));
+  printf("Manual cooking completed!\n");
 }
--- rice_cooker.h
+++ rice_cooker.h
@@ -1,37 +1,35 @@
 #ifndef RICE_COOKER_H
 #define RICE_COOKER_H
 
-struct Content
-{
-    char name[50];
-    double quantity;
+struct Content {
+  char name[50];
+  double quantity;
 };
 
-struct RiceCooker
-{
-    double capacity;
-    struct Content content;
-    int timer;
-    char temperature[50];
-    int cookingDuration;
+struct RiceCooker {
+  double capacity;
+  struct Content content;
+  int timer;
+  char temperature[50];
+  int cookingDuration;
 };
 
-char* getName(struct Content* c);
-double getQuantity(struct Content* c);
-void setName(struct Content* c, const char* newName);
-void setQuantity(struct Content* c, double newQuantity);
-double getCapacity(struct RiceCooker* rc);
-void setCapacity(struct RiceCooker* rc, double newCapacity);
-struct Content getContent(struct RiceCooker* rc);
-void setContent(struct RiceCooker* rc, struct Content newContent);
-void getContentDetails(struct RiceCooker* rc);
-int getTimer(struct RiceCooker* rc);
-void setTimer(struct RiceCooker* rc, int minute);
-char* getTemperature(struct RiceCooker* rc);
-void setTemperature(struct RiceCooker* rc, const char* setupTemperature);
-int getCookingDuration(struct RiceCooker* rc);
-void setCookingDuration(struct RiceCooker* rc, int minute);
-void automaticCook(struct RiceCooker* rc);
-void manualCook(struct RiceCooker* rc);
+char *getName(struct Content *c);
+double getQuantity(struct Content *c);
+void setName(struct Content *c, const char *newName);
+void setQuantity(struct Content *c, double newQuantity);
+double getCapacity(struct RiceCooker *rc);
+void setCapacity(struct RiceCooker *rc, double newCapacity);
+struct Content getContent(struct RiceCooker *rc);
+void setContent(struct RiceCooker *rc, struct Content newContent);
+void getContentDetails(struct RiceCooker *rc);
+int getTimer(struct RiceCooker *rc);
+void setTimer(struct RiceCooker *rc, int minute);
+char *getTemperature(struct RiceCooker *rc);
+void setTemperature(struct RiceCooker *rc, const char *setupTemperature);
+int getCookingDuration(struct RiceCooker *rc);
+void setCookingDuration(struct RiceCooker *rc, int minute);
+void automaticCook(struct RiceCooker *rc);
+void manualCook(struct RiceCooker *rc);
 
 #endif // RICE_COOKER_H
