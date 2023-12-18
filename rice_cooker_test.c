f7653c3356a79acf12e48add75ceaaa121762448
Miaritiana <hei.miaritiana@gmail.com>
2023-12-18 23:16:26 +0300

    feat: add test

---
+++ rice_cooker_test.c
@@ -0,0 +1,59 @@
+#include "gtest/gtest.h"
+#include "rice_cooker.h"
+
+TEST(ContentTest, GetName) 
+{
+    struct Content c;
+    setName(&c, "Rice");
+    EXPECT_STREQ("Rice", getName(&c));
+}
+
+TEST(ContentTest, GetQuantity) 
+{
+    struct Content c;
+    setQuantity(&c, 2.5);
+    EXPECT_DOUBLE_EQ(2.5, getQuantity(&c));
+}
+
+TEST(RiceCookerTest, SetAndGetCapacity) 
+{
+    struct RiceCooker rc;
+    setCapacity(&rc, 5.0);
+    EXPECT_DOUBLE_EQ(5.0, getCapacity(&rc));
+}
+
+TEST(RiceCookerTest, SetAndGetContent) 
+{
+    struct RiceCooker rc;
+    struct Content contents;
+    setName(&contents, "Rice");
+    setCapacity(&rc, 4.0);
+    setQuantity(&contents, 2.0);
+    setContent(&rc, contents);
+
+    struct Content retrievedContent = getContent(&rc);
+    EXPECT_STREQ("Rice", getName(&retrievedContent));
+    EXPECT_DOUBLE_EQ(2.0, getQuantity(&retrievedContent));
+}
+
+TEST(RiceCookerTest, SetAndGetTimer) 
+{
+    struct RiceCooker rc;
+    setTimer(&rc, 30);
+
+    EXPECT_EQ(getTimer(&rc), 30);
+}
+
+TEST(RiceCookerTest, SetAndGetCookingDuration) 
+{
+    struct RiceCooker rc;
+    setCookingDuration(&rc, 30);
+
+    EXPECT_EQ(getCookingDuration(&rc), 30);
+}
+
+int main(int argc, char **argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
